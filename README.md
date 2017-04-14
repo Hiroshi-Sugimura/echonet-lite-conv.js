b18659052fa67b94ccc6360a02d72de6ca157ffe
Hiroshi SUGIMURA <hiroshi.sugimura@gmail.com>
2017-04-14 21:22:36 +0900

    ver 0.0.7

--- README.md
+++ README.md
@@ -3,7 +3,6 @@
 このモジュールは**ECHONET Liteプロトコルのコンバーター**として機能します．
 基本的に，[echonet-lite.js](https://www.npmjs.com/package/echonet-lite)とセットで利用することを想定しています．
 
-
 This module provides **converter for ECHONET Lite protocol**.
 The module to process ECHONET Lite protocol is [here](https://www.npmjs.com/package/echonet-lite).
 
@@ -18,7 +17,7 @@ The module to process ECHONET Lite protocol is [here](https://www.npmjs.com/pack
 
 * An example of a script
 
-    // EL最小構成
+    // EL最小構成, minimum sample
     // ECHONET Liteのコンバーターを準備しておいて
     var ELconv = require('echonet-lite-conv');
 
@@ -117,7 +116,6 @@ The module to process ECHONET Lite protocol is [here](https://www.npmjs.com/pack
 
 * コンバータを初期化しておく（JSON形式の定義データを読む）
 
-
     ELconv.initialize();
 
 
@@ -134,6 +132,8 @@ The module to process ECHONET Lite protocol is [here](https://www.npmjs.com/pack
 
 Demosを見ればだいたいわかると思う．EDTにアクセスするには例えば，
 
+For using this module, data accesing sample is following.
+
     ip  = devs.IPs[3];                // '192.168.2.159'
     eoj = devs[ip].EOJs[1];           // 'Node profile01'
     epc = devs[ip][eoj].EPCs[0];      // 'Operation status'
@@ -148,9 +148,54 @@ Demosを見ればだいたいわかると思う．EDTにアクセスするには
 
 Following file is dictionary for ECHONET Lite in JSON format.
 
-    ./node_modules/echonet-lite-conv/deviceObject_G.json
-    ./node_modules/echonet-lite-conv/nodeProfile.json
-    ./node_modules/echonet-lite-conv/superClass_c.json
+    ./node_modules/echonet-lite-conv/Appendix_G/deviceObject_G.json
+    ./node_modules/echonet-lite-conv/Appendix_G/superClass_G.json
+    ./node_modules/echonet-lite-conv/Appendix_H/deviceObject_H.json
+    ./node_modules/echonet-lite-conv/Appendix_H/superClass_H.json
+    ./node_modules/echonet-lite-conv/Appendix_I/deviceObject_I.json
+    ./node_modules/echonet-lite-conv/Appendix_I/superClass_I.json
+    ./node_modules/echonet-lite-conv/Spec_1.11/nodeProfile.json
+    ./node_modules/echonet-lite-conv/Spec_1.12/nodeProfile.json
+
+
+### ECHONET Lite versions
+
+* 2011.12.21 Appendix A (J)
+* 2011.12.21 Spec Ver 1.00 (J)
+* 2012.03.05 Spec Ver 1.01 (E)
+* 2012.03.05 Spec Ver 1.01 (J)
+* 2012.08.02 Appendix B (E)
+* 2012.08.02 Appendix B (J)
+* 2012.09.03 Spec Ver 1.00 (E)
+* 2012.09.13 Appendix A (E)
+* 2013.03.31 Spec Ver 1.10 (E)
+* 2013.03.31 Spec Ver 1.10 (J)
+* 2013.05.31 Appendix C (E)
+* 2013.05.31 Appendix C (J)
+* 2013.10.31 Appendix D (E)
+* 2013.10.31 Appendix D (J)
+* 2014.03.27 Appendix E (J)
+* 2014.06.09 Spec Ver 1.11 (E)
+* 2014.06.09 Spec Ver 1.11 (J)
+* 2014.09.03 Appendix F (E)
+* 2014.09.03 Appendix F (J)
+* 2015.03.29 Appendix F (J) Errata
+* 2015.05.29 Appendix A (J) Errata
+* 2015.05.29 Appendix B (J) Errata
+* 2015.05.29 Appendix C (J) Errata
+* 2015.05.29 Appendix D (J) Errata
+* 2015.05.29 Appendix E (E)
+* 2015.05.29 Appendix E (J) Errata
+* 2015.05.29 Appendix G (E)
+* 2015.05.29 Appendix G (J)
+* 2015.06.16 Appendix G (J) Revised
+* 2015.09.30 Spec Ver 1.12 (J)
+* 2016.03.27 Spec Ver 1.12 (E)
+* 2016.05.27 Appendix H (E)
+* 2016.05.27 Appendix H (J)
+* 2016.08.24 Appendix H (J) Errata
+* 2016.12.09 Appendix I (J)
+* 2017.03.24 Spec Ver 1.12 (J) Errata
 
 
 
@@ -167,6 +212,7 @@ SUGIMURA, Hiroshi
 
 
 ### 辞書データ作成とご協力
+
 
 Ver. 0.06の辞書「deviceObject_G.json」，「nodeProfile.json」，「superClass.json」は，神奈川工科大学スマートハウス研究センターの
 藤田裕之さんが作成した，ECHONET 機器オブジェクト詳細規定　Release G のJSON dataを利用しました。
@@ -186,6 +232,8 @@ SonyCSL/ECHONETLite-ObjectDatabase: Owada : Devices and properties database for 
 
 
 ## Log
+
+0.0.7 辞書ファイルをVer 1.12のNode ProfileとRelease Iに対応した．内部的にはRelease Hも持っている．その関連で辞書の位置が変わった．EDTを少し解釈するようになった．解釈できないEDTはcontentTypeを付けることにした．EPCのF0からFFまでをユーザ定義領域と解釈結果を付けることにした．
 
 0.0.6 辞書ファイルをVer 1.11eのNode ProfileとRelease Gに対応した．日本語版になった．英語版辞書も欲しい．
 
--- index.js
+++ index.js
@@ -9,15 +9,25 @@ const fs = require('fs');
 
 // クラス変数
 var ELconv = {
-  m_dict: {}
+  m_dict: {},
+  m_latestSpec: '1.12',
+  m_latestAppendix:'I'
 };
 
 
 // 辞書の読み込み
 ELconv.initialize = function () {
-	ELconv.m_dictNod = JSON.parse( fs.readFileSync('./node_modules/echonet-lite-conv/nodeProfile.json', 'utf8') );
-	ELconv.m_dictSup = JSON.parse( fs.readFileSync('./node_modules/echonet-lite-conv/superClass.json', 'utf8') );
-	ELconv.m_dictDev = JSON.parse( fs.readFileSync('./node_modules/echonet-lite-conv/deviceObject_G.json', 'utf8') );
+	// 本来はすべての辞書をもって，条件分けすべき
+	// ELconv.m_dictNod = JSON.parse( fs.readFileSync('./node_modules/echonet-lite-conv/Spec_1.11/nodeProfile.json', 'utf8') );
+	ELconv.m_dictNod = JSON.parse( fs.readFileSync('./node_modules/echonet-lite-conv/Spec_1.12/nodeProfile.json', 'utf8') );
+
+	// ELconv.m_dictSup = JSON.parse( fs.readFileSync('./node_modules/echonet-lite-conv/Appendix_G/superClass_G.json', 'utf8') );
+	// ELconv.m_dictSup = JSON.parse( fs.readFileSync('./node_modules/echonet-lite-conv/Appendix_H/superClass_H.json', 'utf8') );
+	ELconv.m_dictSup = JSON.parse( fs.readFileSync('./node_modules/echonet-lite-conv/Appendix_I/superClass_I.json', 'utf8') );
+
+	// ELconv.m_dictDev = JSON.parse( fs.readFileSync('./node_modules/echonet-lite-conv/Appendix_G/deviceObject_G.json', 'utf8') );
+	// ELconv.m_dictDev = JSON.parse( fs.readFileSync('./node_modules/echonet-lite-conv/Appendix_H/deviceObject_H.json', 'utf8') );
+	ELconv.m_dictDev = JSON.parse( fs.readFileSync('./node_modules/echonet-lite-conv/Appendix_I/deviceObject_I.json', 'utf8') );
 };
 
 
@@ -52,13 +62,14 @@ ELconv.toHexArray = function( string ) {
 ELconv.refEOJ = function(eoj) {
 	var ret = eoj = eoj.toUpperCase();
 	if( eoj.substr(0,4) == '0EF0' ) {
-		ret = ELconv.m_dictNod.elObjects["0x0EF0"].objectName + eoj.substr(4,2)  + "(" + eoj + ")";
+		ret = ELconv.m_dictNod.elObjects["0x0EF0"].objectName + eoj.substr(4,2);
 	}
 	if( ELconv.m_dictDev.elObjects["0x"+eoj.substr(0,4)] ) {
-		ret = ELconv.m_dictDev.elObjects["0x"+eoj.substr(0,4)].objectName + eoj.substr(4,2)  + "(" + eoj + ")";
+		ret = ELconv.m_dictDev.elObjects["0x"+eoj.substr(0,4)].objectName + eoj.substr(4,2);
 	}
 	return ret;
 };
+
 
 // EPCを文字列へ
 ELconv.refEPC = function(eoj, epc) {
@@ -66,6 +77,12 @@ ELconv.refEPC = function(eoj, epc) {
 	// console.log( "refEPC: " + eoj + ":" + epc );
 	eoj = eoj.toUpperCase();
 	var ret = epc = epc.toUpperCase();
+
+	// F0からFFまではuser defined
+	upper = epc.substr( 0, 1 );
+	if( upper == 'F' ) {
+		ret = 'ユーザ定義領域(' + epc + ')';
+	}
 
 	// スーパークラスにあるか？
 	if( undefined != ELconv.m_dictSup.elObjects["0x0000"].epcs["0x"+epc] ) { // 指定のEPCはあるか？あればまず確保，機器固有があれば上書き
@@ -103,17 +120,136 @@ ELconv.refer = function( facilities, callback ) {
 		ret.IPs.push(ip);
 		ret[ip] = {"EOJs": []}
 		Object.keys( facilities[ip] ).forEach( function (eoj) { // eoj
-			retEoj = ELconv.refEOJ(eoj);
+			retEoj = ELconv.refEOJ(eoj) + '(' + eoj + ')';
 			ret[ip].EOJs.push( retEoj );
 			ret[ip][retEoj] = { 'EPCs': [] };
 			Object.keys( facilities[ip][eoj] ).forEach( function (epc) { // epc
 				retEpc = ELconv.refEPC(eoj, epc);
 				ret[ip][retEoj].EPCs.push(retEpc);
-				ret[ip][retEoj][retEpc] = facilities[ip][eoj][epc];
+				ret[ip][retEoj][retEpc] = ELconv.parseEDT( eoj, epc, facilities[ip][eoj][epc] ); //edt
 			});
 		}); // eoj
 	}); // ip
 	callback( ret );
+};
+
+
+//////////////////////////////////////////////////////////////////////
+// parse EDT
+// obj = facilities[ip][ eoj ][epc]
+// epc = facilities[ip][eoj][ epc ]
+// edt = facilities[ip][eoj][epc]
+ELconv.parseEDT = function( eoj, epc, edt ) {
+	eoj = eoj.toUpperCase();
+	epc = epc.toUpperCase();
+	edt = edt.toUpperCase();
+
+	var contentRule;
+	var ret;
+
+	// F0からFFまではuser defined
+	upper = epc.substr( 0, 1 );
+	if( upper == 'F' ) {
+		ret = 'ユーザ定義領域(' + edt + ')';
+		return ret;
+	}
+
+	// スーパークラスにあるか？
+	if( undefined != ELconv.m_dictSup.elObjects["0x0000"].epcs["0x"+epc] ) { // 指定のEPCはあるか？あればまず確保，機器固有があれば上書き
+		contentRule = ELconv.m_dictSup.elObjects["0x0000"].epcs["0x"+epc].edt[0].content;
+	}
+
+	if( eoj.substr(0, 4) == '0EF0' ) {  // node profile object
+		var devProps = ELconv.m_dictNod.elObjects["0x"+eoj.substr(0, 4)];
+		if( "0x"+epc in devProps.epcs ) { // 指定のEPCはあるか？
+			contentRule = ELconv.m_dictNod.elObjects["0x"+eoj.substr(0, 4)].epcs["0x"+epc].edt[0].content;
+		}
+	}
+
+	// 各機器固有のEPCか？
+	if( ELconv.m_dictDev.elObjects["0x"+eoj.substr(0, 4)] ) {  // 指定のEOJはあるか？
+		var devProps = ELconv.m_dictDev.elObjects["0x"+eoj.substr(0, 4)];
+		if( "0x"+epc in devProps.epcs ) { // 指定のEPCはあるか？
+			contentRule = ELconv.m_dictDev.elObjects["0x"+eoj.substr(0, 4)].epcs["0x"+epc].edt[0].content;
+		}
+	}
+
+	// 1.content typeをみてみる
+	for (var contentType in contentRule) {
+		switch (contentType){
+		  case 'rawData':			// numericValue
+			ret = edt + "(" + edt + ")";
+			break;
+
+		  case 'numericValue':			// numericValue
+			var val = 0;
+			switch (contentRule.numericValue.integerType) {
+			  case 'Signed':
+				val = parseInt( edt, 16);  // signedにしたい
+				if (val > 127) { val = val - 256 }
+				break;
+			  case 'Unsigned':
+			  default: // エラー返すのめんどくさいのでとりあえずunsignedとしてしまう，ほんとはダメ
+				val = parseInt( edt, 16);
+				break;
+			}
+
+			var mag = 0; // 倍率を10のN乗表記した指数部。省略可。例: -1(x0.1), 2(x100)
+			if( contentRule.numericValue.magnification ) {
+				mag = parseInt(contentRule.numericValue.magnification);
+			}
+
+			var unit = ''; //  単位。省略可。例: "℃"
+			if( contentRule.numericValue.unit ) {
+				unit = contentRule.numericValue.unit;
+			}
+
+			ret = val * Math.pow(10, mag) + unit + "(" + edt + ")";
+			break;
+
+		  case 'level':
+		  case 'bitmap':
+		  case 'customType':
+		  case 'others':
+		  default:
+			ret = contentType + "(" + edt + ")";
+			break;
+		}
+	}
+
+	// 2.keyValuesを持つときはそちらを優先するが，undefinedの時は値を更新しない
+	for (var contentType in contentRule) {
+		if( contentType == 'keyValues' ) {
+
+			if( epc=='D6' ) {			// 3.keyValuesの特殊な形として，自ノードインスタンスリストSを解決する
+				var count = parseInt( edt.substring( 0, 2) );
+				ret = '';
+				for( var i = 0; i<count; i++ ) {
+					var st = edt.substr( i*6+2, 6);
+					ret += ELconv.refEOJ(st) + ',';
+				}
+				ret = ret.slice(0, -1) + '(' + edt + ')';
+			}
+			else if( epc=='D7' ) { // 3.keyValuesの特殊な形として，自ノードクラスリストSを解決する
+				var count = parseInt( edt.substring( 0, 2) );
+				ret = '';
+				for( var i = 0; i<count; i++ ) {
+					var st = edt.substr( i*4+2, 4 );
+					ret += ELconv.refEOJ(st) + ',';
+				}
+				ret = ret.slice(0, -1) + '(' + edt + ')';
+			}
+			// 4.プロパティマップ以外はJSON DBのEDT対応表を参照
+			else if( contentRule.keyValues['0x' + edt] ) {
+				ret = contentRule.keyValues['0x' + edt]  + "(" + edt + ")";
+			}
+		}
+	}
+
+
+
+
+	return ret;
 };
 
 
